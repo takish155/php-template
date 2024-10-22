40335fa1fd9b66d732f4f680ff2a51d64ec632cd
Takish Lim <rimutakeshi155@gmail.com>
2024-10-22 18:23:09 +0900

    day-1

--- helpers.php
+++ helpers.php
@@ -11,3 +11,39 @@ function basePath($path = "")
 {
   return __DIR__ . "/" . $path;
 }
+
+/**
+ * Load the view
+ * 
+ * @param string $name
+ * @return void
+ */
+
+function loadView($name)
+{
+  $path = basePath("views/$name.view.php");
+
+  if (file_exists($path)) {
+    require $path;
+  } else {
+    echo "Path of $name doesn't exist!";
+  }
+}
+
+/**
+ * Load the partials
+ * 
+ * @param string $partialName
+ * @return void
+ */
+
+function loadPartials($partialName)
+{
+  $path = basePath("views/partials/$partialName.php");
+
+  if (file_exists($path)) {
+    require $path;
+  } else {
+    echo "Path of $partialName doesn't exist!";
+  }
+}
